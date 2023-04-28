4d7fe45b1647aad7cad519e9820d49fa602f36f8
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-04-28 23:05:46 +0400

    Added Animation Waiting with Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -9,23 +8,8 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ECommerceClient';
-  constructor(private toastrService : CustomToastrService){
-    toastrService.message("Basliq", "mesaj", {
-      messageType: ToastrMessageType.Error,
-      position: ToastrPosition.BottomRight
-    })
-    toastrService.message("Basliq", "mesaj", {
-      messageType: ToastrMessageType.Info,
-      position: ToastrPosition.BottomLeft
-    })
-    toastrService.message("Basliq", "mesaj", {
-      messageType: ToastrMessageType.Success,
-      position: ToastrPosition.TopRight
-    })
-    toastrService.message("Basliq", "mesaj", {
-      messageType: ToastrMessageType.Warning,
-      position: ToastrPosition.TopLeft
-    })
+  constructor(){
+   
   }
 }
 
---
+++ src/app/base/base.component.ts
@@ -0,0 +1,21 @@
+import { NgxSpinnerService } from "ngx-spinner";
+
+export class BaseComponent {
+
+  constructor(private spinner: NgxSpinnerService) { }
+  showSpinner(spinnerType: SpinnerType) {
+    this.spinner.show(spinnerType);
+
+    setTimeout(() => this.hideSpinner(spinnerType), 1000);
+  }
+  hideSpinner(spinnerType: SpinnerType) {
+    this.spinner.hide(spinnerType);
+  }
+}
+export enum SpinnerType {
+  LineSpinClockwiseFade = "s1",
+  BallAtom = "s2",
+  BallFall = "s3",
+  BallBeat = "s4",
+  LineScalePulseOut = "s5"
+}
