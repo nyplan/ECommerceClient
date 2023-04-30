d5f4071b6a7e2bedc47fd005faf9d1f15553a374
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-04-30 18:44:52 +0400

    Added Custom HttpClient Service

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,17 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent extends BaseComponent implements OnInit{
-constructor(spinner: NgxSpinnerService) {
-  super(spinner);
-}
+export class ProductsComponent extends BaseComponent implements OnInit {
+  constructor(spinner: NgxSpinnerService, private httpClientService: HttpClientService) {
+    super(spinner);
+  }
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallBeat)
+    this.showSpinner(SpinnerType.BallBeat);
+
+    this.httpClientService.get<Product[]>({
+      controller: "products"
+    }).subscribe(data => console.log(data));
+
+    // this.httpClientService.post({
+    //   controller: "products"
+    // }, {
+    //   name: "Koynek",
+    //   stock: 100,
+    //   price: 15
+    // }).subscribe();
+
+    // this.httpClientService.put({
+    //   controller: "products"
+    // }, {
+    //   id: "7f92a69f-a0e3-45a0-9a24-ee3281a8a5d5",
+    //   name: "rengsiz corab",
+    //   stock: 580,
+    //   price: 1
+    // }).subscribe()
+
+    // this.httpClientService.delete({
+    //   controller: "products"
+    // }, "bc231572-ec69-4109-b62a-14b9da5eae0f").subscribe();
+
+    
   }
 }
