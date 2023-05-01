b71b581b57da4d3ec6df1b4c1ae78fd15e4ad44a
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-05-01 23:20:16 +0400

    Added ProductService

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -15,32 +14,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallBeat);
-
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
-
-    // this.httpClientService.post({
-    //   controller: "products"
-    // }, {
-    //   name: "Koynek",
-    //   stock: 100,
-    //   price: 15
-    // }).subscribe();
-
-    // this.httpClientService.put({
-    //   controller: "products"
-    // }, {
-    //   id: "7f92a69f-a0e3-45a0-9a24-ee3281a8a5d5",
-    //   name: "rengsiz corab",
-    //   stock: 580,
-    //   price: 1
-    // }).subscribe()
-
-    // this.httpClientService.delete({
-    //   controller: "products"
-    // }, "bc231572-ec69-4109-b62a-14b9da5eae0f").subscribe();
-
     
   }
 }
