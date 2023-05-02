cae772dce79099e997bf7182a503b6f5056ee00b
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-05-02 22:47:46 +0400

    Testing Fluent Validation

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -10,18 +10,28 @@ import { ProductService } from '../../../../services/common/models/product.servi
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
-export class CreateComponent extends BaseComponent implements OnInit{
+export class CreateComponent extends BaseComponent implements OnInit {
   constructor(spinner: NgxSpinnerService, private productService: ProductService, private alertify: AlertifyService) {
     super(spinner)
   }
-    ngOnInit(): void {
-    }
+  ngOnInit(): void {
+  }
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
     this.showSpinner(SpinnerType.LineScalePulseOut);
     const create_product: Create_Product = new Create_Product();
     create_product.name = name.value;
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
+
+    if (!name.value) {
+      this.alertify.message("Please provide a name", {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      });
+      return;
+    }
+
 
     this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.LineScalePulseOut);
@@ -30,6 +40,12 @@ export class CreateComponent extends BaseComponent implements OnInit{
         messageType: MessageType.Success,
         position: Position.TopRight
       });
+    }, errorMessage => {
+      this.alertify.message(errorMessage, {
+        dismissOthers: true,
+        messageType: MessageType.Error,
+        position: Position.TopRight
+      })
     });
   }
 
