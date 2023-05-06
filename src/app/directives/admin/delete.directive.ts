075a1a7c3849130fdf847f069a9e0221c0b6e6ae
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-05-06 22:15:34 +0400

    Refactoring Delete Directive

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,9 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
-import { ProductService } from 'src/app/services/common/models/product.service';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 declare var $: any;
 
@@ -15,9 +17,10 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _renderer: Renderer2,
-    private productService: ProductService,
+    private httpClientService: HttpClientService,
     private spinner: NgxSpinnerService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private alertifyService: AlertifyService
   ) {
     const img = _renderer.createElement("img");
     img.setAttribute("src", "../../../../../assets/delete.png");
@@ -28,21 +31,39 @@ export class DeleteDirective {
   }
 
   @Input() id: string;
+  @Input() controller: string; 
   @Output() callback: EventEmitter<any> = new EventEmitter();
   @HostListener("click")
   async onClick() {
     this.openDialog(async () => {
       this.spinner.show(SpinnerType.LineSpinClockwiseFade)
       const td: HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate({
+      this.httpClientService.delete({
+        controller: this.controller
+      }, this.id).subscribe(data => {
+        $(td.parentElement).animate({
         opacity: 0,
         left: "+=50",
         height: "toogle"
       }, 700, () => {
         this.callback.emit();
+        this.alertifyService.message("Item successfully removed", {
+          dismissOthers: true,
+          messageType: MessageType.Success,
+          position: Position.TopRight
+        })
         this.spinner.hide(SpinnerType.LineSpinClockwiseFade)
       });
+      },(errorResponse: HttpErrorResponse) => {
+        this.spinner.hide(SpinnerType.LineSpinClockwiseFade);
+        this.alertifyService.message("Something went wrong", {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        });
+       
+      })
+      
     });
    
   }
