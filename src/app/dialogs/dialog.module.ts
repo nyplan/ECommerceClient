a71740d34ed9e94282d7880c35894bb0307272ff
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-06-21 09:54:13 +0400

    Operation to Add Pictures to Products - 1

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -4,17 +4,20 @@ import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
+import { SelectProductImageDialogComponent } from './select-product-image-dialog/select-product-image-dialog.component';
+import { FileUploadModule } from "../services/common/file-upload/file-upload.module";
 
 
 
 @NgModule({
-  declarations: [
-    DeleteDialogComponent,
-    FileUploadDialogComponent
-  ],
-  imports: [
-    CommonModule,
-    MatDialogModule, MatButtonModule
-  ]
+    declarations: [
+        DeleteDialogComponent,
+        SelectProductImageDialogComponent
+    ],
+    imports: [
+        CommonModule,
+        MatDialogModule, MatButtonModule,
+        FileUploadModule
+    ]
 })
 export class DialogModule { }
