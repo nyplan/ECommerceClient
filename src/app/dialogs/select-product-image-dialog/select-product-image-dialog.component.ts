ad0b6a1fa29374c886a2a9de69295b072b65f45c
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-06-28 16:13:37 +0400

    Operation to Add Pictures to Products - 2

--- src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
+++ src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -1,30 +1,77 @@
-import { Component, Inject, Output } from '@angular/core';
+import { Component, Inject, OnInit, Output } from '@angular/core';
 import { BaseDialog } from '../base/base-dialog';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
+import { ProductService } from 'src/app/services/common/models/product.service';
+import { List_Product_Image } from 'src/app/contracts/list_product_image';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
+import { DialogService } from 'src/app/services/common/dialog.service';
+import {
+  DeleteDialogComponent,
+  DeleteState,
+} from '../delete-dialog/delete-dialog.component';
+import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
+
+declare var $: any;
 
 @Component({
   selector: 'app-select-product-image-dialog',
   templateUrl: './select-product-image-dialog.component.html',
   styleUrls: ['./select-product-image-dialog.component.scss'],
 })
-export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> {
-  constructor(dialogRef: MatDialogRef<SelectProductImageDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string) {
+export class SelectProductImageDialogComponent
+  extends BaseDialog<SelectProductImageDialogComponent>
+  implements OnInit
+{
+  constructor(
+    dialogRef: MatDialogRef<SelectProductImageDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string,
+    private productService: ProductService,
+    private spinner: NgxSpinnerService,
+    private dialogService: DialogService
+  ) {
     super(dialogRef);
   }
 
   @Output() options: Partial<FileUploadOptions> = {
-    accept: ".png, .jpg, .jpeg, .gif",
-    action: "upload",
-    controller: "products",
-    explanation: "Choose product image",
+    accept: '.png, .jpg, .jpeg, .gif',
+    action: 'upload',
+    controller: 'products',
+    explanation: 'Choose product image',
     isAdminPage: true,
-    queryString: `id=${this.data}` 
-  }
+    queryString: `id=${this.data}`,
+  };
 
+  images: List_Product_Image[];
+
+  async ngOnInit(): Promise<void> {
+    this.spinner.show(SpinnerType.BallAtom);
+    this.images = await this.productService.readImages(
+      this.data as string,
+      () => this.spinner.hide(SpinnerType.BallAtom)
+    );
+  }
+  async deleteImage(imageId: string, event: any) {
+    this.dialogService.openDialog({
+      componentType: DeleteDialogComponent,
+      data: DeleteState.Yes,
+      afterClosed: async () => {
+        this.spinner.show(SpinnerType.LineScalePulseOut);
+        await this.productService.deleteImage(
+          this.data as string,
+          imageId,
+          () => {
+            this.spinner.hide(SpinnerType.LineScalePulseOut);
+            var card = $(event.srcElement).parent().parent();
+            card.fadeout(500);
+          }
+        );
+      },
+    });
+  }
 }
 
 export enum SelectProductImageState {
-  Close
+  Close,
 }
