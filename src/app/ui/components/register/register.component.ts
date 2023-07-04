f269bcfe9ec2d7a463b3214b97cda3f3f85c731d
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-07-04 12:26:46 +0400

    Installing Identity Mechanism and Adding Users

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,50 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import { User } from '../../../../app/entities/user';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { Create_User } from '../../../contracts/users/create_user';
+import { User } from '../../../entities/user';
+import { UserService } from '../../../services/common/models/user.service';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from '../../../services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private toastrService: CustomToastrService
+  ) {}
 
   frm: FormGroup;
 
   ngOnInit(): void {
-    this.frm = this.formBuilder.group({
-      adSoyad: ["", [
-        Validators.required,
-        Validators.maxLength(50),
-        Validators.minLength(3)
-      ]],
-      kullaniciAdi: ["", [
-        Validators.required,
-        Validators.maxLength(50),
-        Validators.minLength(3)
-      ]],
-      email: ["", [
-        Validators.required,
-        Validators.maxLength(250),
-        Validators.email
-      ]],
-      sifre: ["",
-        [
-          Validators.required
-        ]],
-      sifreTekrar: ["",
-        [
-          Validators.required
-        ]]
-    }, {
-      validators: (group: AbstractControl): ValidationErrors | null => {
-        let sifre = group.get("sifre").value;
-        let sifreTekrar = group.get("sifreTekrar").value;
-        return sifre === sifreTekrar ? null : { notSame: true };
+    this.frm = this.formBuilder.group(
+      {
+        nameSurname: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(50),
+            Validators.minLength(3),
+          ],
+        ],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(50),
+            Validators.minLength(3),
+          ],
+        ],
+        email: [
+          '',
+          [Validators.required, Validators.maxLength(250), Validators.email],
+        ],
+        password: ['', [Validators.required]],
+        passwordConfirm: ['', [Validators.required]],
+      },
+      {
+        validators: (group: AbstractControl): ValidationErrors | null => {
+          let sifre = group.get('password').value;
+          let sifreTekrar = group.get('passwordConfirm').value;
+          return sifre === sifreTekrar ? null : { notSame: true };
+        },
       }
-    })
+    );
   }
 
   get component() {
@@ -52,12 +70,21 @@ export class RegisterComponent implements OnInit {
   }
 
   submitted: boolean = false;
-  onSubmit(data: User) {
+  async onSubmit(user: User) {
     this.submitted = true;
 
-    debugger;
-    if (this.frm.invalid)
-      return;
+    if (this.frm.invalid) return;
 
+    const result: Create_User = await this.userService.create(user);
+    if (result.succeeded)
+      this.toastrService.message(result.message, 'Kullanıcı Kaydı Başarılı', {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight,
+      });
+    else
+      this.toastrService.message(result.message, 'Hata', {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight,
+      });
   }
 }
