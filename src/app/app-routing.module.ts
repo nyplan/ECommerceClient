6b1a6074165d6535ba3e651be7ad304130499c35
Nurlan Khankishiyev <nurlan.xankisiyev17@gmail.com>
2023-07-04 10:08:35 +0400

    Designing User Registration Form (Reactive Form)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,9 @@ const routes: Routes = [
 { path: "baskets", loadChildren: () => import("./ui/components/baskets/baskets.module")
 .then(module => module.BasketsModule) },
 { path: "products", loadChildren: () => import("./ui/components/products/products.module")
-.then(module => module.ProductsModule) }
+.then(module => module.ProductsModule) },
+{ path: "register", loadChildren: () => import("./ui/components/register/register.module")
+.then(module => module.RegisterModule) }
 ];
 
 @NgModule({
